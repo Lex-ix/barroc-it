444acb763638e39acc6c12637c12b5bd642c666d
Lex Krooswijk <d229297@edu.rocwb.nl>
2017-09-25 12:15:33 +0200

    Index.php / css

---
+++ public/index.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="../css/main.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="banner">
+        <div class="container">
+            <h1 class="title">BARROC IT.</h1>
+        </div>
+    </div>
+
+    <nav class="background">
+        <div class="container">
+            <ul class="right">
+                <li><a href="">Home</a></li>
+                <li><a href="">fdasfad</a></li>
+                <li><a href="">greagra</a></li>
+                <li><a href="">oagekyusxj</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="main-content">
+        <section>
+            <div class="container">
+                <h2 class='subhead'>Lorem Ipsum</h2>
+                <h3 class='subhead'>Lorem Ipsum</h3>
+                <p class="bold">
+                    Lorem ipsum dolor sit amet.
+                </p>
+                <p>
+                    consectetur adipiscing elit. Aliquam a auctor ligula, non vehicula ligula. Suspendisse porta nibh lacus, in dictum nibh pulvinar ac. Pellentesque eleifend sit amet diam ut molestie. Pellentesque efficitur at risus non posuere. Donec at congue metus, dapibus congue lacus. Maecenas mollis erat vel maximus pulvinar. Nam consectetur, est id interdum euismod, tellus libero lacinia tortor, ut dapibus arcu enim non leo. Morbi rutrum velit ac mauris convallis, non maximus sem facilisis. Integer bibendum eros dignissim tortor tincidunt molestie. Curabitur ut viverra metus.
+                </p>
+                <h4 class='subhead'>Lorem Ipsum</h4>
+                <p>
+                    consectetur adipiscing elit. Aliquam a auctor ligula, non vehicula ligula. Suspendisse porta nibh lacus, in dictum nibh pulvinar ac. Pellentesque eleifend sit amet diam ut molestie. Pellentesque efficitur at risus non posuere. Donec at congue metus, dapibus congue lacus. Maecenas mollis erat vel maximus pulvinar.
+                </p>
+            </div>
+        </section>
+        <section class="background">
+            <div class="container">
+                <h2 class='subhead'>Lorem Ipsum</h2>
+                <div class="besides">
+                    <div class="item spacing">
+                        <h3 class='subhead'>Lorem Ipsum</h3>
+                        <p class="bold">
+                            Lorem ipsum dolor sit amet.
+                        </p>
+                        <p>
+                            consectetur adipiscing elit. Aliquam a auctor ligula, non vehicula ligula. Suspendisse porta nibh lacus, in dictum nibh pulvinar ac. Pellentesque eleifend sit amet diam ut molestie. Pellentesque efficitur at risus non posuere. Donec at congue metus, dapibus congue lacus. Maecenas mollis erat vel maximus pulvinar. Nam consectetur, est id interdum euismod, tellus libero lacinia tortor, ut dapibus arcu enim non leo. Morbi rutrum velit ac mauris convallis, non maximus sem facilisis. Integer bibendum eros dignissim tortor tincidunt molestie. Curabitur ut viverra metus.
+                        </p>
+                    </div>
+                    <div class="item">
+                        <h3 class='subhead'>Lorem Ipsum</h3>
+                        <p class="bold">
+                            Lorem ipsum dolor sit amet.
+                        </p>
+                        <p>
+                            consectetur adipiscing elit. Aliquam a auctor ligula, non vehicula ligula. Suspendisse porta nibh lacus, in dictum nibh pulvinar ac. Pellentesque eleifend sit amet diam ut molestie. Pellentesque efficitur at risus non posuere. Donec at congue metus, dapibus congue lacus. Maecenas mollis erat vel maximus pulvinar. Nam consectetur, est id interdum euismod, tellus libero lacinia tortor, ut dapibus arcu enim non leo. Morbi rutrum velit ac mauris convallis, non maximus sem facilisis. Integer bibendum eros dignissim tortor tincidunt molestie. Curabitur ut viverra metus.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="container">
+                <h2 class="subhead">Table</h2>
+                <table>
+                    <tr>
+                        <th>Thing 1 Lorem Ipsum</th>
+                        <th>Thing 2 Lorem Ipsum</th>
+                        <th>Thing 3 Lorem Ipsum</th>
+                    </tr>
+                    <tr>
+                        <td>Thing 1</td>
+                        <td>Thing 2</td>
+                        <td>Thing 3</td>
+                    </tr>
+                    <tr>
+                        <td>Thing 1</td>
+                        <td>Thing 2</td>
+                        <td>Thing 3</td>
+                    </tr>
+                    <tr>
+                        <td>Thing 1</td>
+                        <td>Thing 2</td>
+                        <td>Thing 3</td>
+                    </tr>
+                    <tr>
+                        <td>Thing 1</td>
+                        <td>Thing 2</td>
+                        <td>Thing 3</td>
+                    </tr>
+                    <tr>
+                        <td>Thing 1</td>
+                        <td>Thing 2</td>
+                        <td>Thing 3</td>
+                    </tr>
+                </table>
+            </div>
+        </section>
+        <footer class="center background">
+            <h1 class="subhead">SOFTWARE FOR REAL</h1>
+            <p>© BARROC IT | 2017</p>
+        </footer>
+    </div>
+</html>
